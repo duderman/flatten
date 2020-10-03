c6adf793a80c575352bace79c3c98cd43e5327b2
Nikolay Norkin <nikolay.norkin@forwardpmx.com>
2020-10-03 14:35:44 +0100

    Implementation

--- lib/flatten.ex
+++ lib/flatten.ex
@@ -1,18 +1,27 @@
 defmodule Flatten do
   @moduledoc """
-  Documentation for `Flatten`.
+  Flatten is responsible for flattening lists
   """
 
+  @spec flatten(any) :: {:ok, list()} | {:error, :not_a_list}
   @doc """
-  Hello world.
+  flatten/1 method flattens a given list
 
   ## Examples
 
-      iex> Flatten.hello()
-      :world
+      iex> Flatten.flatten([ 1, [ 2, [ 3 ] ], 4 ])
+      {:ok, [ 1, 2, 3, 4 ]}
+
+      iex> Flatten.flatten("asd")
+      {:error, :not_a_list}
 
   """
-  def hello do
-    :world
+  def flatten(arg) when not is_list(arg), do: {:error, :not_a_list}
+  def flatten(list), do: {:ok, do_flatten(list)}
+
+  defp do_flatten([head | tail]) when is_list(head) do
+    do_flatten(head) ++ do_flatten(tail)
   end
+  defp do_flatten([head | tail]), do: [head | do_flatten(tail)]
+  defp do_flatten(item), do: item
 end
--- test/flatten_test.exs
+++ test/flatten_test.exs
@@ -2,7 +2,17 @@ defmodule FlattenTest do
   use ExUnit.Case
   doctest Flatten
 
-  test "greets the world" do
-    assert Flatten.hello() == :world
+  test "flatten/1 flattens a list" do
+    assert Flatten.flatten([ 1, [ 2, [ 3 ] ], 4 ]) == {:ok, [ 1, 2, 3, 4 ]}
+  end
+
+  test "flatten/1 flattens an empty list" do
+    assert Flatten.flatten([]) == {:ok, []}
+  end
+
+  test "flatten/1 raises an error when given something other than a list" do
+    assert Flatten.flatten("asdasd") == {:error, :not_a_list}
+    assert Flatten.flatten(1) == {:error, :not_a_list}
+    assert Flatten.flatten({:ok, :not_ok}) == {:error, :not_a_list}
   end
 end
