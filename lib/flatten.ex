8d1a6447f3f96c7ed1f29cbfdff76a4751ef6430
Nikolay Norkin <nikolay.norkin@forwardpmx.com>
2020-10-03 14:13:27 +0100

    Initial commit

---
+++ lib/flatten.ex
@@ -0,0 +1,18 @@
+defmodule Flatten do
+  @moduledoc """
+  Documentation for `Flatten`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Flatten.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
